66e04428f7b2d091151527a42a312581fbf8a165
Geo <qj2023@nyu.edu>
2024-07-28 22:11:58 +0800

    Facing left&right

--- MetroVaniaDemo2/Assets/Scripts/PlayerController.cs
+++ MetroVaniaDemo2/Assets/Scripts/PlayerController.cs
@@ -3,37 +3,48 @@ using System.Collections;
 
 
 public class ExampleClass : MonoBehaviour {
-    [SerializeField]
-    private float speed = 10.0f;
-    public float jumpForce = 10.0f;
-    public float rotationSpeed = 100.0f;
-    public float xInput ;
-    public float yInput ;
-    public Rigidbody2D rb;
-
-    float horizontalSpeed = 12.0f;
-    float verticalSpeed = 12.0f;
+    [SerializeField] private float horizontalSpeed = 10.0f;
+    [SerializeField] private float jumpForce = 10.0f;
+    [SerializeField] private float rotationSpeed = 100.0f;
+    [SerializeField] private float xInput ;
+    [SerializeField] private float yInput ;
+    
+    private Rigidbody2D rb;
+    private Animator player_AC;
 
     void Start() {
         Application.targetFrameRate = 60;
         rb = GetComponent<Rigidbody2D>();
+        player_AC = GetComponentInChildren<Animator>();
     }
 
     void Update() {
         xInput = Input.GetAxis("Horizontal");
-        yInput = Input.GetAxis("Vertical");
-        //transform.position = new Vector3(transform.position.x + xInput * verticalSpeed * Time.deltaTime, 
-        //transform.position.y, transform.position.z);
-        Move();
+        //yInput = Input.GetAxis("Vertical");
+        CheckInput();
     }
 
-    void Move(){
-        if (Input.GetButton("Horizontal")){
-            rb.velocity = new Vector2 (xInput* horizontalSpeed, rb.velocity.y);
+    void CheckInput(){
+        if (Input.GetButton("Horizontal")) {
+            Movement();
         }
-        else if (Input.GetKeyDown(KeyCode.Space)){
-            //rb.AddForce(new Vector2(0, jumpForce));
-            rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
+        else if (Input.GetKeyDown(KeyCode.Space)) {
+            Jump();
         }
+    }
+
+    private void Movement()
+    {
+        rb.AddForce(new Vector2(xInput * horizontalSpeed, rb.velocity.y));
+        if (rb.velocity.x != 0)
+        {
+            player_AC.SetBool("isMoving", true);
+        }
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(new Vector2(0, jumpForce));
+        //rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
     }
 }
