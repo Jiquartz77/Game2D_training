b9592e002375906d595d69c540f37c83ad091107
Geo <qj2023@nyu.edu>
2024-07-31 00:22:12 +0800

    Dash&CoolDown

--- MetroVaniaDemo2/Assets/Scripts/PlayerController.cs
+++ MetroVaniaDemo2/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,14 @@ public class ExampleClass : MonoBehaviour {
     [SerializeField] private float horizontalSpeed = 10.0f;
     [SerializeField] private float jumpForce = 10.0f;
     [SerializeField] private float xInput ;
-    [SerializeField] private float yInput ;
+    [SerializeField] private float velocity_x;
+    [SerializeField] private float dashDuration =0.7f;
+    [SerializeField] private float dashTimer =0;
+    [SerializeField] private float dashSpeed = 30.0f;
+    [SerializeField] private float dashCoolDownDuration = 2.0f;
+    [SerializeField] private float dashCoolDownTimer = 0f;
+    [SerializeField] private bool canDash = true;
+
     
     private Rigidbody2D rb;
     private Animator player_AC;
@@ -28,25 +35,43 @@ public class ExampleClass : MonoBehaviour {
 
     void Update() {
         xInput = Input.GetAxis("Horizontal");
-        //yInput = Input.GetAxis("Vertical");
+
+        dashTimer -= Time.deltaTime;
+        dashCoolDownTimer -= Time.deltaTime;
+        if (dashTimer < -10){ dashTimer %= 10; }
+        if (dashCoolDownTimer < -10){ dashTimer %= 10; }
+        velocity_x = rb.velocity.x;
+
         CheckInput();
         CollisionChecks();
         AnimatorController();
         FacingFlipController();
     }
 
+
     void CheckInput(){
         Movement();
         if (Input.GetButtonDown("Horizontal")) { }
         else if (Input.GetKeyDown(KeyCode.Space)) {
             Jump();
+        }else if (Input.GetKeyDown(KeyCode.LeftShift)){
+            Dash();
         }
+
     }
 
     private void Movement() {
-        //rb.AddForce(new Vector2(xInput * horizontalSpeed, 0));
-        rb.velocity = new Vector2 (xInput * horizontalSpeed, rb.velocity.y);
-        //if (rb.velocity.x != 0) { player_AC.SetBool("isMoving", true); }
+        if (dashTimer > 0 && canDash){
+            rb.velocity = new Vector2 (facingDirection * dashSpeed, 0);
+        }else {
+            rb.velocity = new Vector2 (xInput  * horizontalSpeed, rb.velocity.y);
+            //rb.AddForce(new Vector2(xInput * horizontalSpeed, 0));
+        }
+    }
+
+    private void Dash(){
+        dashTimer = dashDuration;
+        dashCoolDownTimer = dashCoolDownDuration;
     }
 
     private void Jump() {
@@ -61,6 +86,8 @@ public class ExampleClass : MonoBehaviour {
         bool isMoving = (rb.velocity.x != 0);
         player_AC.SetBool("isMoving", isMoving);
         player_AC.SetBool("isGrounded", isGrounded);
+        player_AC.SetFloat("yVelocity", rb.velocity.y);
+        player_AC.SetBool("isDashing", dashTimer > 0);
     }
 
     private void FacingFlip(){
@@ -83,4 +110,10 @@ public class ExampleClass : MonoBehaviour {
     private void CollisionChecks(){
         isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
     }
+
+/*     private void FixedUpdate() {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            if (isGrounded) rb.AddForce(transform.up * jumpForce);
+        }
+    } */
 }
