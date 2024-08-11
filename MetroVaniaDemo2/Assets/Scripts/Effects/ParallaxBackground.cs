bdb44c4323964e4e7b12f6ea3aa03631bc8c1111
Geo <qj2023@nyu.edu>
2024-08-11 23:29:32 +0800

    Tilemap&Cinemachine

---
+++ MetroVaniaDemo2/Assets/Scripts/Effects/ParallaxBackground.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParrallaxBackground : MonoBehaviour {
+    private GameObject cameraDIY;
+    [SerializeField] private float parallaxEffect;
+
+    private float xPosition;
+    private float length;
+}
