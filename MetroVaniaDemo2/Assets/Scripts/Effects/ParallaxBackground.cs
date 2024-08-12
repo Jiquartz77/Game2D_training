cd63a7cfbf3c20add22d4484cb1d69245901e79b
Geo <qj2023@nyu.edu>
2024-08-12 23:58:34 +0800

    Endless Background. Enemy template created

--- MetroVaniaDemo2/Assets/Scripts/Effects/ParallaxBackground.cs
+++ MetroVaniaDemo2/Assets/Scripts/Effects/ParallaxBackground.cs
@@ -3,9 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ParrallaxBackground : MonoBehaviour {
-    private GameObject cameraDIY;
+    private GameObject mainCamera;
     [SerializeField] private float parallaxEffect;
 
     private float xPosition;
     private float length;
+
+    void Start() {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        xPosition = transform.position.x;
+
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void Update() {
+        float distanceToMove = mainCamera.transform.position.x * parallaxEffect;
+        float distanceMoved = mainCamera.transform.position.x - distanceToMove;
+
+        transform.position = new Vector3(xPosition + distanceToMove, transform.position.y);
+
+        if (distanceMoved > xPosition + length || distanceMoved < xPosition - length){
+            xPosition += length;
+        }
+
+    }
 }
---
+++ MetroVaniaDemo2/Assets/Scripts/Enemy/EnemyState.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyState {
+    protected Enemy enemyBase;
+    protected EnemyStateMachine stateMachine;
+    protected Rigidbody2D rb;
+
+    protected float stateTimer;
+
+    protected bool triggerCalled;
+    private string animBoolName;
+
+    public EnemyState(Enemy enemyBase, EnemyStateMachine stateMachine,
+     string animBoolName) {
+        this.enemyBase = enemyBase;
+        this.animBoolName = animBoolName;
+        this. stateMachine = stateMachine;
+    }
+
+    public virtual void Enter() {
+        triggerCalled =false;
+        rb = enemyBase.rb;
+        enemyBase.anim.SetBool(animBoolName, true);
+    }
+
+    public virtual void Update() {
+        stateTimer -= Time.deltaTime;
+    }
+    
+    public virtual void Exit() {
+        triggerCalled =true;
+        enemyBase.anim.SetBool(animBoolName, true);
+    }
+}
